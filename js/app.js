030080190ea2b5edef736b7b4287577e88c7a8d4
djrclaudio12 <djrclaudio12@gmail.com>
2026-02-13 12:32:05 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ yes.addEventListener("click", () => {
   vid.style.display = "block";
   gif.src = "../resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Knew it baby 😘";
-  yes.innerHTML = '<a href="https://askmyval.com/v/xGoJVRLZtaweiy3RvhlJ">Message for you</a>';// inside the " " put your social media profile link
+  yes.innerHTML = '<a href="https://askmyval.com/v/78xuGtwKXhqxA71pZjZA">Message for you My Love</a>';// inside the " " put your social media profile link
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
@@ -66,6 +66,7 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
+
 
 
 
